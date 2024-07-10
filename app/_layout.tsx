a36e48236a9a34e3cee1725057e45e8a81c2e9fb
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-07-11 01:33:28 +0300

    Wrapped RootLayout component to include SafeAreaProvider and SafeAreaView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { AuthProvider } from './AuthProvider';
 import { Stack } from 'expo-router';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
     return (
-        <AuthProvider>
-            <Stack>
-                <Stack.Screen name="index" options={{ headerShown: false }} />
-                <Stack.Screen name="login/index" options={{ headerShown: false }} />
-                <Stack.Screen name="register/index" options={{ headerShown: false }} />
-                <Stack.Screen name="todos/index" options={{ headerShown: false }} />
-            </Stack>
-        </AuthProvider>
+        <SafeAreaProvider>
+            <AuthProvider>
+                <SafeAreaView style={{ flex: 1, backgroundColor: '#43CFC6' }}>
+                    <Stack>
+                        <Stack.Screen name="index" options={{ headerShown: false }} />
+                        <Stack.Screen name="login/index" options={{ headerShown: false }} />
+                        <Stack.Screen name="register/index" options={{ headerShown: false }} />
+                        <Stack.Screen name="todos/index" options={{ headerShown: false }} />
+                    </Stack>
+                </SafeAreaView>
+            </AuthProvider>
+        </SafeAreaProvider>
     );
 }
